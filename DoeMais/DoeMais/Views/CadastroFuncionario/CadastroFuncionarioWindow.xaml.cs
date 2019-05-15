7b1c5a3357b025f142aec9f842113df3e7aeee9d
David Lima <david.lima50@etec.sp.gov.br>
2019-05-15 19:57:26 -0300

    fixed CadastroFuncionarioWindow

---
+++ DoeMais/DoeMais/Views/CadastroFuncionario/CadastroFuncionarioWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DoeMais.Views.CadastroFuncionario
+{
+    /// <summary>
+    /// Interaction logic for CadastroFuncionarioWindow.xaml
+    /// </summary>
+    public partial class CadastroFuncionarioWindow : Window
+    {
+        public CadastroFuncionarioWindow()
+        {
+            InitializeComponent();
+            MinimizeWindow.Click += (s, e) => WindowState = WindowState.Minimized;
+            CloseApp.Click += (s, e) => ControlViews.closePerfil();
+        }
+    }
+}
